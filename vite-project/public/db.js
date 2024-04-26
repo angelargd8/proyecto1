30946a045c968d5ee8573b3710dc615c530aa413
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-04-26 17:52:23 -0600

    xd

---
+++ vite-project/public/db.js
@@ -0,0 +1,61 @@
+import conn from './conn.js';
+
+/*
+GET /posts que retorna un listado de todos los posts. Debe retornar un array y un status 200 si fue exitoso.
+GET /posts/:postId que retorna el detalle de un post con el id postId. Debe retornar un objeto y un status 200 si fue exitoso.
+POST /posts que permite crear un nuevo post. Debe retornar un objeto con el post creado y un status 200 si fue exitoso.
+PUT /posts/:postId  que permite modificar un post. Debe retornar un objecto con el post editado y un status 200 si exitoso.
+DELETE /posts/:postId que borra un post. No debe retornar contenido y debe retornar status 204.
+*/
+
+
+async function getAllPosts() {
+    const [rows] = await conn.query('SELECT * FROM blogs')
+    return rows
+   }
+   
+   
+   async function createPost(title, content,  descripcion, imagen) {
+       const [result] = await conn.query('INSERT INTO blogs (title, content, descripcion, imagen) VALUES (?, ?, ?, ?)', [title, content,  descripcion, imagen])
+       return result
+   }
+   
+   async function getPostId(id){
+       const [result] = await conn.query('SELECT * FROM blogs WHERE id = ?', [id])
+       return result
+   }
+   
+   async function updatePost(id, title, content,  descripcion, imagen){
+       const [result] = await conn.query('UPDATE blogs SET title = ?, content = ?,  descripcion = ?, imagen = ? WHERE id = ?', [title, content,  descripcion, imagen, id])
+       return result
+   }
+   
+   //Eliminar Post
+   async function deletePost(id){
+       const [result] = await conn.query('DELETE FROM blogs WHERE id = ?', [id])
+       return result
+   }
+   
+   
+   async function createPost2(funcion, informacion) {
+       const [result] = await conn.query('INSERT INTO blog_db2.funciones (funcion, informacion) VALUES (?, ?)', [funcion, informacion])
+       return result
+   }
+   
+   async function getAllPostsFunctions() {
+       const [rows] = await conn.query('SELECT * FROM blog_db2.funciones')
+       return rows
+      }
+      
+   
+export {
+    getAllPosts,
+    createPost, 
+    getPostId,
+    updatePost,
+    deletePost,
+    createPost2,
+    getAllPostsFunctions
+}
+   
+    //validarConexion()
---
+++ vite-project/public/main.js
@@ -0,0 +1,160 @@
+import express from 'express';
+import cors from 'cors';
+import * as db from './db.js';
+import swaggerUi from 'swagger-ui-express';
+import path from 'path';
+//import yaml from 'js-yaml';
+import { load as yamlLoad } from 'js-yaml';
+import url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+//middleware para json Aca es donde se usa los cors
+app.use(cors());
+
+app.use(express.json())
+
+//cargar el archivo YAML que describe endpoints
+//const swaggerDocument = yaml.load('./swagger.yaml')
+const swaggerDocument = yamlLoad('./swagger.yaml')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', (req, res) => {
+  //res.send('Hello World from Express!')
+  res.sendFile(path.join(__dirname, '../index.html'))
+  console.log('Hello World from Express!')
+})
+
+
+app.get('/index.html', (req, res) => {
+  //mandarle aqui el index
+  res.sendFile(path.join(__dirname, '../index.html'))
+  console.log('Hello World from Express!')
+})
+
+app.get('/index', (req, res) => {
+  //mandarle aqui el index
+  res.sendFile(path.join(__dirname, '../index.html'))
+  console.log('Hello World from Express!')
+})
+
+
+app.get('/funciones', (req, res) => {
+  res.send('Hello World from Express!')
+})
+
+app.get('/blogs', (req, res) => {
+  res.send('Hello World from Express!')
+})
+
+app.get('/type', (req, res) => {
+  res.send('Hello World from Express!')
+})
+
+
+//--get all posts
+app.get('/posts/info', async (req, res) => {
+  const posts = await db.getAllPosts()
+  if(posts.length === 0) {
+    res.status(200).json({message: 'Empty state: No posts found'})
+  }else{
+    res.status(200).json(posts)
+  } 
+  console.log('posts', posts)
+})
+
+
+//--get all posts from  functions
+app.get('/posts/f', async (req, res) => {
+  const posts = await db.getAllPostsFunctions()
+  if(posts.length === 0) {
+    res.status(200).json({message: 'Empty state:No posts found'})
+  }else{
+    res.status(200).json(posts)
+  } 
+  console.log('posts', posts)
+})
+
+//--create post
+app.post('/posts', cors({ origin: 'http://127.0.0.1:5173' }), async (req, res) => {
+  try{
+    console.log(req)
+    const { title, content, descripcion, imagen } = req.body
+    const newPost = await db.createPost(title, content, descripcion, imagen)
+    res.json({ message: 'Post created' })//
+    res.status(200).json(newPost)
+
+    console.log('Post created')
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
+
+
+//--create post 2
+app.post('/posts/p', cors({ origin: 'http://127.0.0.1:5173' }), async (req, res) => {
+  try{
+    console.log(req)
+    const { funcion, informacion } = req.body
+    const newPost = await db.createPost2(funcion, informacion )
+    res.json({ message: 'Post created' })//
+    res.status(200).json(newPost)
+
+    console.log('Post created')
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+  
+})
+
+//-get post by id
+app.get('/posts/:postId', async (req, res) => {
+  const { id } = req.params
+  const post = await db.getPostId(id)
+  res.status(200).json(post)
+  //console.log('posts', posts)
+})
+
+//--update post
+app.put('/posts/:postId', async (req, res) => {
+  const { id } = req.params
+  const { title, content, titulo, descripcion, imagen } = req
+  const updatedPost = await db.updatePost(id, title, content, titulo, descripcion, imagen)
+  if (!updatedPost) {
+    res.status(400).json({ message: 'Post not found' })
+  } else {
+    res.status(200).json(updatedPost)
+    res.json({ message: 'Post updated' })
+  }
+
+})
+//--delete post
+app.delete('/post/:postId', async (req, res) => {
+  const { id } = req.params
+  const deletedPost = await db.deletePost(id)
+  res.status(204).json(deletedPost)
+  res.json({ message: 'Post deleted' })
+  console.log('Post deleted')
+
+})
+
+
+//inicio del server
+const port = 5173
+app.listen(port, () => {
+  //console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server listening at http://127.0.0.1:${port}`)
+})
+
+//validacion de implementacion de http 
+
+app.use((req, res) => {
+  res.status(501).send('error 501: metodo no implementad')
+})
+
+
+export default app;
