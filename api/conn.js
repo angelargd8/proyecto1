0d842fdbeb1b7634933a1941d91db602b2a40920
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-05-06 09:58:08 -0600

    ahora si, ya esta bn el puerto

--- api/conn.js
+++ api/conn.js
@@ -2,14 +2,14 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    //host: 'localhost',
     user: 'root',
     database: 'blog_db2',
     password: 'root_password',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    port: 33068
+    port: 3306
 })
 
 //export default pool
